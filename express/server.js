598a099854d80b930cd3338dc3708fca9476cd33
Jacob Hansil <jacobhansil@gmail.com>
2018-05-20 02:11:58 -0400

    launch config and server demo update

--- express/server.js
+++ express/server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const app = express();
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
 
 
-app.get('/api/v1/getit', (req,res) => 
-  MongoClient.connect('mongodb+srv://jhansil:pa77word@pathmaster-ac6vm.mongodb.net/test', function (err, db) {
+app.get('/api/v1/getit', (req,res) => {
+  MongoClient.connect('mongodb+srv://jhansil:pa77word@pathmaster-ac6vm.mongodb.net/test', (err, db) => {
     if (err) throw err;
-    var dbo = db.db('test');
-    dbo.collection('customers').findOne({name: 'Company Inc'},(function (err, result) {
+    let dbo = db.db('test');
+    dbo.collection('customers').findOne({name: 'Company Inc'}, (err, result) => {
       if (err) throw err;
 
       console.log(result.name);
       res.send(result.name);
       db.close();
-    }));
-  })
-);
+    });
+  });
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.get('/quit', function(req,res) {
+  res.send('closing...');
+  server.close();
+});
+
+let server = app.listen(3000, () => console.log('Example app listening on port 3000!'));
