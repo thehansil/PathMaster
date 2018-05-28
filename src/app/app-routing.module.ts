3c8042ec0f3ea521eef9c29834c98a978d06ef49
Jacob Hansil <jacobhansil@gmail.com>
2018-05-27 21:11:22 -0400

    reorganized file hierarchy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CharactersComponent } from './characters/characters.component';
-import { PdfViewerComponent } from './dashboard/pdf-viewer/pdf-viewer.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { PdfViewerComponent } from './components/dashboard/pdf-viewer/pdf-viewer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 // Components
-import { AppComponent } from './app.component';
-import { DiceRollComponent } from './dice-roll/dice-roll.component';
-import { PdfViewerComponent } from './dashboard/pdf-viewer/pdf-viewer.component';
-import { CharactersComponent } from './characters/characters.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { InitiativeComponent } from './dashboard/initiative/initiative.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppComponent } from './components/app.component';
+import { DiceRollComponent } from './components/dice-roll/dice-roll.component';
+import { PdfViewerComponent } from './components/dashboard/pdf-viewer/pdf-viewer.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { AppRoutingModule } from './app-routing.module';
+import { InitiativeComponent } from './components/dashboard/initiative/initiative.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
